638ebd52462c0a7043571aa55f9dccc74be50605
batista29 <natabatista2908@gmail.com>
2024-03-11 17:07:46 -0300

    Primeira aula de python - PUCC

---
+++ puc2024.1/algoritmos-proj-comp/aula03/exercicios.py
@@ -0,0 +1,91 @@
+#Transforma um número em impar
+'''Esse programa le um numero inteiro e se
+ele for par soma 1 para transformar em impar'''
+
+
+
+'''EXERCICIO 1'''
+
+'''num=int(input("Digite um número inteiro:"))
+
+if (num%2==0):
+    num=num+1
+    print(f"O valor do número foi alterado e é {num}")
+else:
+    print(f"o valor continua o mesmo e é {num}")'''
+
+
+'''EXERCICIO 2'''    
+
+'''lado1=int(input("Digite o valor do lado superior do seu objeto: "))
+lado2=int(input("Digite o valor do lado esquerdo do seu objeto: "))
+
+if(lado1 == lado2):
+    print("É um quadrado")
+else:
+    print("É um retângulo")'''
+
+    
+'''EXERCICIO 3'''
+
+'''num1=int(input("Digite um número inteiro: "))
+num2=int(input("Digite um número inteiro que seja diferente do ja digitado: "))
+
+if(num1 > num2):
+    print(f"{num1} é maior que {num2}")
+else:
+    print(f"{num2} é maior que {num1}")'''
+
+
+'''EXERCICIO 4'''
+
+'''num=int(input("Digite um número"))
+if(num>=0):
+    print("O número é positivo")
+elif num < 0:
+    print("O número é negativo")
+else:
+    print("O número é nulo")'''
+
+
+'''EXERCICIO 5'''
+
+'''num=int(input("Digite um numero inteiro: "))
+if(num%5==0):
+    print("O número é multiplo de 5")
+else:
+    print("Não é multiplo de 5")'''
+        
+
+'''EXERCICIO 6'''
+
+'''a=int(input("Digite um numero inteiro"))
+b=int(input("Digite um numero inteiro"))
+
+if(a == b):
+    c = a+b
+else:
+    c = a*b
+print(f"O valor da conta é: {c}")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
