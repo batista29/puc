1c0e54c5c891c0befe85e32ebe6639648b8d53c8
Natã Batista <98469905+batista29@users.noreply.github.com>
2024-03-27 11:27:38 -0300

    Add files via upload

--- puc2024.1/algoritmos-proj-comp/aula05/ex2.py
+++ puc2024.1/algoritmos-proj-comp/aula05/ex2.py
@@ -1,13 +1,13 @@
 idade50=0
 somaAlturas=0
 qntsIdade=0
-while(0<25):
+while(0<5):
     idade = int(input("Digite a idade"))
     altura = float(input("Digite a altura"))
     peso = int(input("Digite o peso"))
     somaAlturas = somaAlturas+altura
     if(idade>=10 and idade<=20):
-    qntsIdade = qntsIdade+1
+        qntsIdade = qntsIdade+1
     if(idade>50):
         idade50=idade50+1
     print(idade50)
