dcc16d2c6825380745b609e8f711628578ae73f5
batista29 <natabatista2908@gmail.com>
2024-03-28 18:46:54 -0300

    add: exercicio de fatorial feito

--- puc2024.1/algoritmos-proj-comp/aula05/ex2.py
+++ puc2024.1/algoritmos-proj-comp/aula05/ex2.py
@@ -2,16 +2,13 @@ num_maior = int(input("Digite um numero inteiro"))
 valor_soma=0
 valor=0
 
-if(num_maior == 1 or num_maior == 0):
-    valor_soma=1
-else:
-    while (num_maior > 0):
-        if (valor_soma <= 0):
-            num_menor = num_maior - 1
-            valor = num_maior * num_menor
-            valor_soma = valor_soma + valor
-            num_maior = num_menor - 1
-        else:
-            valor_soma = valor_soma * num_maior
-            num_maior = num_maior - 1
+while (num_maior > 0):
+    if (valor_soma <= 0):
+        num_menor = num_maior - 1
+        valor = num_maior * num_menor
+        valor_soma = valor_soma + valor
+        num_maior = num_menor - 1
+    else:
+        valor_soma = valor_soma * num_maior
+        num_maior = num_maior - 1
 print(valor_soma)
