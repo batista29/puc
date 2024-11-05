2331f0e6ad48b701e0330f0c8dfa6e9861037069
Natã Batista <98469905+batista29@users.noreply.github.com>
2024-11-05 10:51:14 -0300

    Banco da modelagem de Clinica adicionado

---
+++ puc2024.2/bcd/sql/clinica.sql
@@ -0,0 +1,38 @@
+CREATE TABLE especialidade(
+  codigo INTEGER PRIMARY KEY,
+  nome VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE medico(
+  crm INTEGER PRIMARY KEY,
+  salario decimal(5,2) NOT NULL,
+  nome VARCHAR(20) NOT NULL,
+  cod_especialidade INTEGER NOT NULL,
+  foreign KEY(cod_especialidade) references especialidade(codigo)
+);
+
+CREATE TABLE quarto(
+  numero INTEGER NOT NULL PRIMARY KEY,
+  andar INTEGER NOT NULL
+);
+
+CREATE TABLE paciente(
+ cpf VARCHAR(11) NOT NULL PRIMARY KEY,
+ rg VARCHAR(11) NOT NULL,
+ endereco VARCHAR(30) NOT NULL,
+ nome VARCHAR(20) NOT NULL,
+ telefone VARCHAR(13) NOT NULL,
+ data_nascimento date NOT NULL,
+ numero_quarto INTEGER NOT NULL,
+ crm_medico INTEGER NOT NULL,
+ foreign KEY (numero_quarto) references quarto(numero),
+ hora_atendimento timestamp NOT NULL,
+ foreign key(crm_medico) references medico(crm)
+);
+
+CREATE TABLE atende(
+  crm_medico INTEGER NOT NULL,
+  cpf_paciente VARCHAR(11) NOT NULL,
+  foreign key(cpf_paciente) references paciente(cpf),
+  primary key (crm_medico, cpf_paciente)
+);
